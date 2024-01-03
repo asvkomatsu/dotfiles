78001659852f0ab08981a885840fd449c7fa86a6
André Komatsu <asuzuki@ananta.home.lan>
2024-01-03 14:04:18 -0300

    misc fixes

--- dotfiles/screenlayout/monitor.sh
+++ dotfiles/screenlayout/monitor.sh
@@ -6,6 +6,15 @@ xrandr \
 	--output HDMI-A-0 --mode 3840x2160 --pos 3840x0 --rotate normal \
 	--output DisplayPort-0 --off \
 	--output DisplayPort-1 --off
+{%@@ elif profile == "ananta" @@%}
+xrandr \
+	--output HDMI-0 --primary --mode 3840x2160 --pos 0x0 --rotate normal \
+	--output DP-4 --mode 3840x2160 --pos 3840x0 --rotate normal \
+	--output DP-0 --off \
+	--output DP-1 --off \
+	--output DP-2 --off \
+	--output DP-3 --off \
+	--output DP-5 --off
 {%@@ elif profile == "DTI-12052" @@%}
 xrandr \
 	--output DP-1 --off \
--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,20 +1,29 @@
 # install packages
 sudo apt-get update
 sudo apt-get install \
+	7zip \
 	apt-transport-https \
 	aptitude \
+	arandr \
 	clang \
 	cmake \
 	curl \
 	dotdrop \
 	fd-find \
 	firmware-realtek \
+	foliate \
 	fonts-liberation \
 	htop \
 	libgdk-pixbuf2.0-0 \
-	libu2f-dev \
+	libu2f-udev \
+	lxappearance \
+	mcomix \
+	mpv \
+	mupdf \
 	nala \
 	remmina \
 	ripgrep \
 	tree \
+	unrar \
+	vlc \
 	vim
